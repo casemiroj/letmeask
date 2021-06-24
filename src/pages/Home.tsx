be285e112932222125f147720f0f7cca0500d1df
casemiro <jv.casemiro@hotmail.com.br>
2021-06-24 17:16:51 -0300

    Encerrar sala

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,7 +33,12 @@ export function Home() {
     const roomRef = await database.ref(`rooms/${roomCode}`).get();
 
     if(!roomRef.exists()) {
-      alert('Room does not exists.')
+      alert('A sala não existe.')
+      return
+    }
+
+    if(roomRef.val().endedAt) {
+      alert('A sala foi fechada.')
       return
     }
 
